73982e12ce08f3fbd5a7640516de2c403f504130
js <342853167@qq.com>
2022-02-19 20:21:52 +0800

    first commit

---
+++ jscloud-gateway-gateway9527/src/main/java/com/js/zkja/jsspringcloud/GatewayMain9527_1.java
@@ -0,0 +1,19 @@
+package com.js.zkja.jsspringcloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author EiletXie
+ * @Since 2020/3/12 13:59
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527_1 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayMain9527_1.class,args);
+	}
+}
